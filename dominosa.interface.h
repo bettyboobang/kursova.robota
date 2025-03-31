fb274f88318063a5ea3515ded7f5cfbbf07f47a5
bettyboobang <zorasolaryefi@gmail.com>
2025-03-31 23:16:10 +0300

    Додані хедерні файли для вирішення головоломки та інтерфейс. Також todo таски в кожен файл програми.

---
+++ dominosa.interface.h
@@ -0,0 +1,11 @@
+#ifndef D_INTERFACE.H
+#define D_INTERFACE
+
+class dominosa_interface {
+public:
+	virtual ~dominosa_interface() = default;//для правильного звільнення ресурсів тому що використовуємо поліморфізм
+	virtual bool solve() = 0; // метод розвязання яки йще буде реалізований в дочірніх класах
+	virtual void show_solution() const = 0;  //також метод для дочірнього класу проте для показу розвязку
+};
+
+#endif
---
+++ solvedominosa.h
@@ -0,0 +1,34 @@
+#ifndef D_SOLVE_H
+#define D_SOLVE_H
+#include "dominosa.interface.h"
+#include "board.h"
+
+#include <vector>
+#include <map>
+#include <utility> //для пар тобто обєктів що можуть тримати в собі два різних типи значень
+using namespace std;
+
+namespace dominosa {//тут я представляю координати на полі
+   struct coordinate {
+   	int row;
+   	int col;
+   	coordinate(int row, int col) : row(row), col(col) {}   	
+   };
+}
+class dominosa_solve : public dominosa_interface {
+     board d_board;
+     int num_max;
+     map <pair<int, int>, bool> domino_avaliable;//для відтеження доступних доміно
+     vector <pair<dominosa::coordinate, dominosa::coordinate>> find_solution //зберігає знайдений розвязок ака координати доміно
+     vector <vector<bool>> is_cover;//а це для відстеження покритих кліт.поля
+     
+     void init_dominosa(); //для ініціалізаціх набору всіх доміно з можливих
+     void super_solve(); // для розвязання головолом.
+     pair<int, int> grab_next_cell(); //непокрита клітинка для спроби розмістити доміно
+public:
+	dominosa_solve(const board &b);
+	bool solve() override();//перевизначення компілятор перепірить чи є з такою ж назіою віртуальна фукнція
+	void show_solution() const override();
+};
+
+#endif
